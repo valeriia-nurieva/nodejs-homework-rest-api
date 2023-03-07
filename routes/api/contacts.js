9051513598b695b68694cee607a454fdb271c97d
valeriia-nurieva <valerka.0906@gmail.com>
2023-03-07 23:09:05 +0200

    add Mongoose

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,17 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const ctrl = require('../../controllers/contacts');
-const { validateBody } = require('../../middlewares');
-const { addSchema } = require('../../schemas/contacts');
+const { validateBody, isValidId } = require('../../middlewares');
+const { schemas } = require('../../models/contact');
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post("/", validateBody(addSchema), ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
 router.delete("/:contactId", ctrl.deleteById);
 
-router.put("/:contactId", validateBody(addSchema), ctrl.updateById);
+router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
+
+router.patch("/:contactId/favorite", validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-// const { DB_HOST } = require('./config');
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
-console.log(process.env.DB_HOST);
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
     });
   })
   .catch((error) => {
