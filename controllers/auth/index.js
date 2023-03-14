fc77cb21da5b07c14242ac216e997b0ef98fa4d4
valeriia-nurieva <valerka.0906@gmail.com>
2023-03-14 20:26:56 +0200

    adds the ability to upload a user avatar

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,11 +2,13 @@ const register = require("./register");
 const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
+const updateAvatar = require("./updateAvatar");
 const { ctrlWrapper } = require("../../helpers");
 
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
     getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout)
+    logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar)
 };
---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+const { User } = require('../../models');
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+
+    await Jimp.read(`${tempUpload}`)
+        .then(image => {
+            return image
+                .resize(250, 250)
+                .writeAsync(`${tempUpload}`);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    
+    res.json({ avatarURL });
+};
+
+module.exports = updateAvatar;
