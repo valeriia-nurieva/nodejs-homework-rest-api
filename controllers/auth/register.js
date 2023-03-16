89dd0486905afc80b27dd4a39de705481f9a3c33
valeriia-nurieva <valerka.0906@gmail.com>
2023-03-16 16:11:41 +0200

    adds email verification

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,11 @@
 const bcrypt = require('bcryptjs');
 const gravatar = require("gravatar");
 const { User } = require('../../models');
-const { HttpError } = require('../../helpers');
+const { HttpError, sendEmail } = require('../../helpers');
+const { v4: uuidv4 } = require('uuid');
+require("dotenv").config();
+
+const { BASE_URL } = process.env;
 
 const register = async (req, res, next) => {
     const { email, password } = req.body;
@@ -11,9 +15,18 @@ const register = async (req, res, next) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
-    const avatarURL = gravatar.url(email); 
+    const avatarURL = gravatar.url(email);
+    const verificationToken = uuidv4();
 
-    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken });
+    const verifyEmail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`,
+    };
+
+    await sendEmail(verifyEmail);
+
     res.status(201).json({
         email: newUser.email,
         name: newUser.name,
